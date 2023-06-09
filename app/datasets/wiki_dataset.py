0473f0a7699bac5339ae57e78b1ef3dd1155dd1e
SadiaAfrinPurba <purba0101@gmail.com>
2023-06-10 00:52:02 +0600

    Select subset of data to fit the model

--- app/datasets/wiki_dataset.py
+++ app/datasets/wiki_dataset.py
@@ -1,16 +1,20 @@
 from torch.utils.data import Dataset
 
 class WikiDataset(Dataset):
-    def __init__(self, file_path, tokenizer, max_length):
+    def __init__(self, file_path, tokenizer, max_length, is_taking_subset, data_subset_size):
         self.file_path = file_path
         self.tokenizer = tokenizer
         self.max_length = max_length
+        self.is_taking_subset = is_taking_subset
+        self.data_subset_size = data_subset_size
         self.data = []
 
         with open(self.file_path, 'r', encoding='utf-8') as file:
             for line in file:
                 sentence = line.strip()
                 self.data.append(sentence)
+        if self.is_taking_subset:
+            self.data = self.data[:data_subset_size]
 
     def __len__(self):
         return len(self.data)
