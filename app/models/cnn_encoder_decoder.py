cd27aef74222b7acc3c4d3121b6567063203371d
SadiaAfrinPurba <purba0101@gmail.com>
2023-06-12 01:02:58 +0600

    Change the linear layer input size for both encoder and decoder

--- app/models/cnn_encoder_decoder.py
+++ app/models/cnn_encoder_decoder.py
@@ -8,10 +8,11 @@ class CNNEncoder(Encoder):
     def __init__(self, input_dim, latent_dim):
         super(CNNEncoder, self).__init__()
 
-        self.conv1 = nn.Conv1d(input_dim, 32, kernel_size=3, padding=1) # {(768-3)+1} {(32-3)+1}
+        self.conv1 = nn.Conv1d(input_dim, 32, kernel_size=3, padding=1) # {(128 - 3 +2)+1}
         self.conv2 = nn.Conv1d(32, 64, kernel_size=3, padding=1)
         self.flatten = nn.Flatten()
-        self.fc = nn.Linear(latent_dim, 64 * input_dim)
+        self.fc_mu = nn.Linear(64*128, 32) # 8192, 32 (latent)
+        self.fc_logvar = nn.Linear(64 * 128, 32)  # 8192, 32 (latent)
         self.relu = nn.ReLU()
 
     def forward(self, x):
@@ -20,17 +21,18 @@ class CNNEncoder(Encoder):
         x = self.relu(self.conv1(x)) # (64, 32, 128)
         x = self.relu(self.conv2(x)) # (64, 64, 128)
         x = self.flatten(x) # (64,8192) 64*128=8192
-        x = x.t() # (8192, 64)
-        x = self.fc(x)
-        return x # (8192, 49152)
+        # x = x.t() # (8192, 64)
+        mu = self.fc_mu(x)
+        log_var = self.fc_logvar(x)
+
+        return mu, log_var
 
 
 class CNNDecoder(Decoder):
     def __init__(self, latent_dim, output_dim):
         super(CNNDecoder, self).__init__()
-
-        self.fc = nn.Linear(64*128, (output_dim * 64) // 2)
-        self.unflatten = nn.Unflatten(1, (64, output_dim // 2))
+        self.fc = nn.Linear(32, 64 * 128)
+        self.unflatten = nn.Unflatten(1, (64, 128))
         self.deconv1 = nn.ConvTranspose1d(64, 32, kernel_size=3, padding=1)
         self.deconv2 = nn.ConvTranspose1d(32, output_dim, kernel_size=3, padding=1)
         self.relu = nn.ReLU()
@@ -38,7 +40,7 @@ class CNNDecoder(Decoder):
 
     def forward(self, x):
         # x -> (8192, 24576)
-        x = x.t()
+        # x = x.t()
         x = self.relu(self.fc(x))
         x = self.unflatten(x)
         x = self.relu(self.deconv1(x))
