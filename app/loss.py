dc2ba1976a341af92d56181a190363877c42ebe8
Sadia Afrin Purba <purba0101@gmail.com>
2023-10-30 17:19:32 +0600

    Update loss.py

--- app/loss.py
+++ app/loss.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 
 def calculate_vae_loss(x, x_hat, mu, logvar, kl_coefficient):
-    reconstruction_loss = nn.functional.binary_cross_entropy(x_hat, x, reduction='sum')
+    reconstruction_loss = nn.MSELoss()(x_hat, x)
     kl_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
     total_loss = reconstruction_loss + kl_loss * kl_coefficient
     return total_loss
